3adad9587aabb00cafc6cdcfe69b390e954a40dd
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-12 11:45:25 -0700

    Update and rename versatile.points to versatile.points.R

---
+++ versatile.points.R
@@ -0,0 +1,15 @@
+#soc
+versatile.points<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab,ylab,colour){
+  ifelse(colour=="topo",
+    points(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
+    cex=cex,xlab=xlab,ylab=ylab,
+    col=topo.colors(length(data[which(data.window>thresh,arr.in=T)]))[rank(data[which(data.window>thresh,arr.in=T)])],
+    xlim=xlim, ylim=ylim),
+    points(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
+    cex=cex,xlab=xlab,ylab=ylab,
+    col=colour,
+    xlim=xlim,
+    ylim=ylim)
+  )
+}
+#eoc
