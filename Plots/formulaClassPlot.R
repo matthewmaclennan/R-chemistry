40661c1d0a6aa81af4f27018d4d1e8308e96bace
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-13 21:19:54 -0700

    Update formulaClassPlot.R

--- Plots/formulaClassPlot.R
+++ Plots/formulaClassPlot.R
@@ -1,5 +1,7 @@
 #EDC-NA losses of 71, then 116. Also, loss of whole EDC is at the bottom.
-plot(naC30O2N3Z32[which(naC30O2N3Z32[,"Z-number"]<=(-4) & naC30O2N3Z32[,"C"]>=9 & naC30O2N3Z32[,"N"]==3 & naC30O2N3Z32[,"Charge"]==1 & naC30O2N3Z32[,"O"]==2),"m/z"],cex=1,col="red",pch=2,ylim=c(50,500),xlim=c(0,300))
-points(naC30O2N3Z32[which(naC30O2N3Z32[,"Z-number"]<=(-2) & naC30O2N3Z32[,"C"]>=6 & naC30O2N3Z32[,"N"]==2 & naC30O2N3Z32[,"Charge"]==1 & naC30O2N3Z32[,"O"]==1),"m/z"]-71,cex=1,col="blue",pch=3)
-points(naC30O2N3Z32[which(naC30O2N3Z32[,"Z-number"]<=(-2) & naC30O2N3Z32[,"C"]>=4 & naC30O2N3Z32[,"N"]==1 & naC30O2N3Z32[,"Charge"]==1 & naC30O2N3Z32[,"O"]==1),"m/z"]-116,cex=1,col="dark green",pch=4)
-points(naC30O2N3Z32[which(naC30O2N3Z32[,"Z-number"]<=(-2) & naC30O2N3Z32[,"C"]>=1 & naC30O2N3Z32[,"N"]==1 & naC30O2N3Z32[,"Charge"]==1 & naC30O2N3Z32[,"O"]==1),"m/z"]-173,cex=1,col="orange",pch=5)
+naEDClossPlot<-function(namatrix){
+plot(namatrix[which(namatrix[,"Z-number"]<=(-4) & namatrix[,"C"]>=9 & namatrix[,"N"]==3 & namatrix[,"Charge"]==1 & namatrix[,"O"]==2),"m/z"],cex=1,col="red",pch=2,ylim=c(50,500),xlim=c(0,300))
+points(namatrix[which(namatrix[,"Z-number"]<=(-2) & namatrix[,"C"]>=6 & namatrix[,"N"]==2 & namatrix[,"Charge"]==1 & namatrix[,"O"]==1),"m/z"]-71,cex=1,col="blue",pch=3)
+points(namatrix[which(namatrix[,"Z-number"]<=(-2) & namatrix[,"C"]>=4 & namatrix[,"N"]==1 & namatrix[,"Charge"]==1 & namatrix[,"O"]==1),"m/z"]-116,cex=1,col="dark green",pch=4)
+points(namatrix[which(namatrix[,"Z-number"]<=(-2) & namatrix[,"C"]>=1 & namatrix[,"N"]==1 & namatrix[,"Charge"]==1 & namatrix[,"O"]==1),"m/z"]-173,cex=1,col="orange",pch=5)
+}
