c6099983bdb79bdd66e5a36f05744280d2172943
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-14 00:03:04 -0700

    Update versatile.plot.match.R

--- Plots/versatile.plot.match.R
+++ Plots/versatile.plot.match.R
@@ -1,1 +1,8 @@
-plot(mpzA11[which(mpzA11[mpzA11[,2] %in% mzu,]>8e5,arr.in=T)[,1]]~mpzA11[1,][which(mpzA11[mpzA11[,2] %in% mzu,]>8e5,arr.in=T)[,2]],cex=0.1,col="red")
+#Special plotting
+versatile.plot.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab=""){
+  plot(data[which(data[data[,2] %in% mzvec,]>thresh,arr.in=T)[,1]]~data[1,][which(data[data[,2] %in% mzvec,]>thresh,arr.in=T)[,2]],cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab)
+}
+#Special plotting
+versatile.points.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab=""){
+  points(data[which(data[data[,2] %in% mzvec,]>thresh,arr.in=T)[,1]]~data[1,][which(data[data[,2] %in% mzvec,]>thresh,arr.in=T)[,2]],cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab)
+}
