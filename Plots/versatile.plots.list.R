b01cc0af597ac055c346e95dfb50d20540b84999
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 15:12:05 -0700

    Update versatile.plots.list.R

--- Plots/versatile.plots.list.R
+++ Plots/versatile.plots.list.R
@@ -2,11 +2,11 @@
 #
 versatile.plot.list<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour){
   ifelse(colour=="topo",
-    plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3]))),
+    plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3])),
     cex=cex,xlab=xlab,ylab=ylab,
-    col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,1])))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))],
+    col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,1])))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,1])))]),
     xlim=xlim, ylim=ylim),
-    plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3]))),
+    plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3])),
     cex=cex,xlab=xlab,ylab=ylab,
     col=colour,
     xlim=xlim,
