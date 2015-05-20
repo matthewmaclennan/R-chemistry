0d4284fdd8a04daa9a25e085b77384d184bdbc07
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 12:45:00 -0700

    Update nagenFormula.R

--- Generators/nagenFormula.R
+++ Generators/nagenFormula.R
@@ -1,6 +1,12 @@
-nagenFormula<-function(nagenresult,derivstr){
+nagenFormula<-function(nagenresult,loss,derivstr){
 formula<-paste0("C",nagenresult[,"C"],"H",nagenresult[,"H"],"N",nagenresult[,"N"],"O",nagenresult[,"O"],"  Z=",nagenresult[,"Z-number"])
-underivFormulaChain<-paste0(derivstr,"C",(nagenresult[,"C"]-9),"H",(nagenresult[,"H"]-19),"N",(nagenresult[,"N"]-3),"O",(nagenresult[,"O"]-2),"  Z=",(nagenresult[,"Z-number"]-(-4)))
-Rchain<-paste0("C",(nagenresult[,"C"]-9),"H",(nagenresult[,"H"]-19),"  Z=",(nagenresult[,"Z-number"]-(-4)))
+nagenLoss<-t(apply(nagenresult,1,function(x) x-loss))
+underivFormulaChain<-paste0("C",nagenLoss[,"C"],"H",nagenLoss[,"H"],"N",nagenLoss[,"N"],"O",nagenLoss[,"O"],"  Z=",nagenLoss[,"Z-number"])
+Rchain<-paste0("C",nagenLoss[,"C"],"H",nagenLoss[,"H"],"  Z=",nagenLoss[,"Z-number"])
 matrix(c(formula,underivFormulaChain,Rchain),ncol=3)
 }
+#References for calculating the NA hydrocarbon chain formulae
+#Loss of EDC and amide C(=O) c(0,-4,9,19,2,3,0,0,0)
+#Loss of EDC neutral fragment 71 (remove the diamine left) c()
+#Loss of EDC neutral fragment 71 and neutral fragment 45 (remove oxazinium left)  c()
+#Loss of N,N-DMEDA and amide C(=O) c(0,0,5,12,1,2,0,0,0)
