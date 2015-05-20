324bf123f03166439b1b149ae6761fe58a6ef27f
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 12:03:17 -0700

    Update nagenFormula.R

--- Generators/nagenFormula.R
+++ Generators/nagenFormula.R
@@ -1,5 +1,6 @@
 nagenFormula<-function(nagenresult,derivstr){
 formula<-paste0("C",nagenresult[,"C"],"H",nagenresult[,"H"],"N",nagenresult[,"N"],"O",nagenresult[,"O"],"  Z=",nagenresult[,"Z-number"])
 underivFormulaChain<-paste0(derivstr,"C",(nagenresult[,"C"]-9),"H",(nagenresult[,"H"]-19),"N",(nagenresult[,"N"]-3),"O",(nagenresult[,"O"]-2),"  Z=",(nagenresult[,"Z-number"]-(-4)))
-matrix(c(formula,underivFormulaChain),ncol=2)
+R-chain<-paste0("C",(nagenresult[,"C"]-9),"H",(nagenresult[,"H"]-19),"  Z=",(nagenresult[,"Z-number"]-(-4)))
+matrix(c(formula,underivFormulaChain,R-chain),ncol=3)
 }
