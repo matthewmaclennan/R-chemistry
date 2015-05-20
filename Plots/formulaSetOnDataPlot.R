be27b4521b79d1bf170805d517a55e6b49b6be4e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 16:38:09 -0700

    Update formulaSetOnDataPlot.R

--- Plots/formulaSetOnDataPlot.R
+++ Plots/formulaSetOnDataPlot.R
@@ -2,11 +2,25 @@
 #formulaset is a subset of the matrix produced by nagenmz
 formulaSetOnDataPlot<-function(data,formulaset,thresh,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour){
   ifelse(colour=="topo",
-    plot(unlist(lapply(data,function(x) x[x[,2]>thresh & x[,1][round(x[,1],0) %in% formulaset[,"m/z"]],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & x[,1][round(x[,1],0) %in% formulaset[,"m/z"]],3])),
+    plot(unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],3])),
     cex=cex,xlab=xlab,ylab=ylab,
     col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,2]))))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,2])))],
     xlim=xlim, ylim=ylim),
-    plot(unlist(lapply(data,function(x) x[x[,2]>thresh & x[,1][round(x[,1],0) %in% formulaset[,"m/z"]],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & x[,1][round(x[,1],0) %in% formulaset[,"m/z"]],3])),
+    plot(unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],3])),
+    cex=cex,xlab=xlab,ylab=ylab,
+    col=colour,
+    xlim=xlim,
+    ylim=ylim)
+    )
+}
+
+formulaSetOnDataPoints<-function(data,formulaset,thresh,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour){
+  ifelse(colour=="topo",
+    points(unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],3])),
+    cex=cex,xlab=xlab,ylab=ylab,
+    col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,2]))))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,2])))],
+    xlim=xlim, ylim=ylim),
+    points(unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],1]))~unlist(lapply(data,function(x) x[x[,2]>thresh & round(x[,1],0) %in% formulaset[,"m/z"],3])),
     cex=cex,xlab=xlab,ylab=ylab,
     col=colour,
     xlim=xlim,
