5a14fc2de3e5e87f64ec9b305a00a7fc0c650eca
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 15:32:50 -0700

    Update versatile.plots.list.R

--- Plots/versatile.plots.list.R
+++ Plots/versatile.plots.list.R
@@ -4,7 +4,7 @@ versatile.plot.list<-function(data,thresh,cex,xlim=NULL,ylim=NULL,xlab="",ylab="
   ifelse(colour=="topo",
     plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3])),
     cex=cex,xlab=xlab,ylab=ylab,
-    col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,1])))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,1])))]),
+    col=topo.colors(length(unlist(lapply(data,function(x) x[x[,2]>thresh,2]))))[rank(unlist(lapply(data,function(x) x[x[,2]>thresh,2])))],
     xlim=xlim, ylim=ylim),
     plot(unlist(lapply(data,function(x) x[x[,2]>thresh,1]))~unlist(lapply(data,function(x) x[x[,2]>thresh,3])),
     cex=cex,xlab=xlab,ylab=ylab,
