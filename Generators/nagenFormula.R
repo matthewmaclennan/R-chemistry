44904652f8ccaa6d9ee9b32eb7c36b8ca2a8dc46
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-20 13:42:26 -0700

    Update nagenFormula.R

--- Generators/nagenFormula.R
+++ Generators/nagenFormula.R
@@ -7,6 +7,6 @@ matrix(c(formula,underivFormulaChain,Rchain),ncol=3)
 }
 #References for calculating the NA hydrocarbon chain formulae
 #Loss of EDC and amide C(=O) c(0,-4,9,19,2,3,0,0,0)
-#Loss of EDC neutral fragment 71 (remove the diamine left) c()
-#Loss of EDC neutral fragment 71 and neutral fragment 45 (remove oxazinium left)  c()
+#Loss of EDC neutral fragment 71 (remove the diamine left) c(0,-2,6,14,1,2,0,0,0)
+#Loss of EDC neutral fragment 71 and neutral fragment 45 (remove oxazinium left)  c(0,-4,4,6,1,1,0,0,0)
 #Loss of N,N-DMEDA and amide C(=O) c(0,0,5,12,1,2,0,0,0)
