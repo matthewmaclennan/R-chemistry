8edc4545f1a9575db30dfc477e1fcae14a467a18
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-01 15:01:06 -0700

    Update Surfaces.R

--- Response-Surface/Surfaces.R
+++ Response-Surface/Surfaces.R
@@ -16,23 +16,28 @@ currentz.poly<-lm(currentzlm[,3]~I(pwidthlm[,1])+I(pwidthlm[,2])+I(pwidthlm[,1]*
 atimez.poly<-lm(atimezlm[,3]~I(pwidthlm[,1])+I(pwidthlm[,2])+I(pwidthlm[,1]*pwidthlm[,2])+I((pwidthlm[,1])^2)+I((pwidthlm[,2])^2))
 #
 #
+pwidthlmw<-cbind(pwidthlm[,-3],apply(pwidthlm[,-3],1,function(x) 100-sum(x)),pwidthlm[,3])
+swathzlmw<-cbind(swathzlm[,-3],apply(swathzlm[,-3],1,function(x) 100-sum(x)),swathzlm[,3])
+migzlmw<-cbind(migzlm[,-3],apply(migzlm[,-3],1,function(x) 100-sum(x)),migzlm[,3])
+currentzlmw<-cbind(currentzlm[,-3],apply(currentzlm[,-3],1,function(x) 100-sum(x)),currentzlm[,3])
+atimezlmw<-cbind(atimezlm[,-3],apply(atimezlm[,-3],1,function(x) 100-sum(x)),atimezlm[,3])
 #Add in the columns for % water and approximate pH. Function percent2pH() is available as *.R file also at
 #www.github.com/matthewmaclennan/R-chemistry/Response-Surface/percent2pH.R
-pwidthlmw<-cbind(pwidthlm[,-3],apply(pwidthlm[,-3],1,function(x) 100-sum(x)),
+pwidthlmwph<-cbind(pwidthlm[,-3],apply(pwidthlm[,-3],1,function(x) 100-sum(x)),
 -logb(t(apply(pwidthlmw,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
 pwidthlm[,3])
 #
 #
 #
-swathzlmw<-cbind(swathzlm[,-3],apply(swathzlm[,-3],1,function(x) 100-sum(x)),
--logb(t(apply(swathzlm,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
+swathzlmwph<-cbind(swathzlm[,-3],apply(swathzlm[,-3],1,function(x) 100-sum(x)),
+-logb(t(apply(swathzlmw,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
 swathzlm[,3])
-migzlmw<-cbind(migzlm[,-3],apply(migzlm[,-3],1,function(x) 100-sum(x)),
--logb(t(apply(migzlm,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
+migzlmwph<-cbind(migzlm[,-3],apply(migzlm[,-3],1,function(x) 100-sum(x)),
+-logb(t(apply(migzlmw,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
 migzlm[,3])
-currentzlmw<-cbind(currentzlm[,-3],apply(currentzlm[,-3],1,function(x) 100-sum(x)),
--logb(t(apply(currentzlm,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
+currentzlmwph<-cbind(currentzlm[,-3],apply(currentzlm[,-3],1,function(x) 100-sum(x)),
+-logb(t(apply(currentzlmw,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
 currentzlm[,3])
-atimezlmw<-cbind(atimezlm[,-3],apply(atimezlm[,-3],1,function(x) 100-sum(x)),
--logb(t(apply(atimezlm,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
+atimezlmwph<-cbind(atimezlm[,-3],apply(atimezlm[,-3],1,function(x) 100-sum(x)),
+-logb(t(apply(atimezlmw,1,function(x) percent2pH(x[2],1.22,46,1.77e-4,x[3])))[,1],10),
 atimezlm[,3])
