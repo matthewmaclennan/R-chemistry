eb48b406c5ea0db31aa55e16c9b12ba170f3c957
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-01 12:11:11 -0700

    Update versatile.plot.R

--- Plots/versatile.plot.R
+++ Plots/versatile.plot.R
@@ -4,15 +4,15 @@
 #The viewing window can be adjusted (data.window)
 #
 #
-versatile.plot<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour){
+versatile.plot<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour,pch=""){
   ifelse(colour=="topo",
     plot(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
-    cex=cex,xlab=xlab,ylab=ylab,
+    cex=cex,xlab=xlab,ylab=ylab,pch=pch,
     col=topo.colors(length(data[which(data.window>thresh,arr.in=T)]))[rank(data[which(data.window>thresh,arr.in=T)])],
     xlim=xlim, ylim=ylim),
     plot(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
     cex=cex,xlab=xlab,ylab=ylab,
-    col=colour,
+    col=colour,pch=pch,
     xlim=xlim,
     ylim=ylim)
     )
