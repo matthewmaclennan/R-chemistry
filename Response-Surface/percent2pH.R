569dfb0f825396c01a4f0594ddae78260d78ee24
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-01 14:35:53 -0700

    Update percent2pH.R

--- Response-Surface/percent2pH.R
+++ Response-Surface/percent2pH.R
@@ -1,5 +1,7 @@
 #Convert volume percentage of solvent to pH based on monoprotic acid dissociation and no organic solvent effects
-percent2pH<-function(acidperc, acid_density,acid_molarmass,waterperc){
+percent2pH<-function(acidperc, acid_density,acid_molarmass, acidKa,waterperc){
   acid_init_molarity<-acidperc*acid_density*acid_molarmass/waterperc
-  ICEtable<-
+  ICEtable<-matrix(c(acid_init_molarity*acidKa,-acidKa,-1),ncol=1)
+  roots<-polyroot(ICEtable)
+  final<-as.numeric(roots)
 }
