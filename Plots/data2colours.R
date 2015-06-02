c3bd07375ae74da21b360c7618e02a192d4c5746
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-01 22:50:06 -0700

    Update data2colours.R

--- Plots/data2colours.R
+++ Plots/data2colours.R
@@ -1,6 +1,6 @@
 #Convert data to a list of colours
 data2colours<-function(data,data.window,thresh,rampPalette){
 
-rampPalette(length(data[which(data.window>thresh,arr.in=T)]))[rank(data[which(data.window>thresh,arr.in=T)])]")
+rampPalette(length(data[which(data.window>thresh,arr.in=T)]))[rank(data[which(data.window>thresh,arr.in=T)])]"
 
 }
